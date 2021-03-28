8596a6c015a08b95dd0146a0487d150c518fca8a
LAPTOP-5K218H50\Supercom - 1 <ritthirach.kongjarit@mail.kmutt.ac.th>
2021-03-28 21:22:30 +0700

    ++Fix the typos

--- Third Practical Midterm Test/Third Practical Midterm Test/Program.cs
+++ Third Practical Midterm Test/Third Practical Midterm Test/Program.cs
@@ -9,9 +9,19 @@ namespace Third_Practical_Midterm_Test
             int _1stBox, _2ndBox, _3rdBox, aTake, bTake;
             do
             {
-                Console.Write("Please input the amount of thing is 1st Box; ");
+                Console.Write("Please input the amount of thing in 1st Box; ");
                 _1stBox = int.Parse(Console.ReadLine());
             } while (_1stBox <= 0);
+            do
+            {
+                Console.Write("Please input the amount of thing in 2nd Box; ");
+                _2ndBox = int.Parse(Console.ReadLine());
+            } while (_2ndBox <= 0);
+            do
+            {
+                Console.Write("Please input the amount of thing in 1st Box; ");
+                _3rdBox = int.Parse(Console.ReadLine());
+            } while (_3rdBox <= 0);
         }
     }
 }
