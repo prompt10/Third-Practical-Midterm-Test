807bbba87c00ee625e717f75f10cbab5e816c647
LAPTOP-5K218H50\Supercom - 1 <ritthirach.kongjarit@mail.kmutt.ac.th>
2021-03-28 21:21:25 +0700

    ++Start the project ++Create THE THREE  boxes OF DELUSION

---
+++ Third Practical Midterm Test/Third Practical Midterm Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Third_Practical_Midterm_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int _1stBox, _2ndBox, _3rdBox, aTake, bTake;
+            do
+            {
+                Console.Write("Please input the amount of thing is 1st Box; ");
+                _1stBox = int.Parse(Console.ReadLine());
+            } while (_1stBox <= 0);
+        }
+    }
+}
